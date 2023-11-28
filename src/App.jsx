7498deadc16f380f2b300920c5a1c7cb2516a24f
Snowli11 <62450413+Snowli11@users.noreply.github.com>
2023-11-28 11:35:34 +0000

    Customized site

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,39 @@
-/**
- * Application component
- *
- * To contain application wide settings, routes, state, etc.
- */
-
 import React from "react";
 
 import About from "./Components/About";
 import Footer from "./Components/Footer";
-import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
 
 import "./styles.css";
 
-/**
- * This object represents your information. The project is set so that you
- * only need to update these here, and values are passed a properties to the
- * components that need that information.
- *
- * Update the values below with your information.
- *
- * If you don't have one of the social sites listed, leave it as an empty string.
- */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Pascal Jeiziner",
+  title: "B.Sc. Student in Biomedical Engineering",
+  email: "pjeiziner@gmail.com",
+  gitHub: "Snowli11",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#000000";
+const secondaryColor = "#FFFFFF";
 
 const App = () => {
   return (
-    <div id="main">
-      <Header />
-      <Home name={siteProps.name} title={siteProps.title} />
-      <About />
-      <Portfolio />
-      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
-    </div>
+      <div id="main">
+        <div id="home">
+          <Home name={siteProps.name} title={siteProps.title} />
+        </div>
+        <About />
+        <div style={{ background: "Black" }}>
+          <Portfolio />
+          <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+        </div>
+      </div>
   );
 };
 
